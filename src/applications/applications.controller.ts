99a7c76f4fdab7e5e26ec6c3a77470aac9ab5411
Ritu Anuragi <81502161+rituanuragi@users.noreply.github.com>
2024-09-26 12:06:19 +0530

    loan-application-api-modification

--- src/applications/applications.controller.ts
+++ src/applications/applications.controller.ts
@@ -8,11 +8,11 @@ export class ApplicationsController {
   @Get('get-loan-applications')
   async getLoanApplications(
     @Query('page') page: number = 1,
-    @Query('offset') offset: number = 12,
+    @Query('offset') offset: number = 6,
   ): Promise<any> {
     try {
       const data = await this.applicationsService.getCustomerData(page, offset);
-      return { success: true, data };
+      return { success: true, data: data.data, totalCount: data.totalCount };
     } catch (error) {
       return { success: false, message: error.message };
     }
