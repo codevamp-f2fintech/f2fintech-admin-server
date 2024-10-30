629005caefb865af0ffa173a07438411a95d11a7
coder-faraz <hfaraz316@gmail.com>
2024-10-30 14:18:18 +0530

    added cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Enable CORS globally
   app.enableCors({
-    origin: 'http://localhost:3000', // Allow requests from this origin
+    origin: ['http://localhost:3000', 'https://admin-f2fintech.netlify.app'], // Allow requests from this origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Set to true if you need to allow cookies or authentication headers
   });
