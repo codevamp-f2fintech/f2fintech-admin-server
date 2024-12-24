7ca93069f9d4580dca498fdae1b5d60928fae35c
shnawaz844 <168626974+shnawaz844@users.noreply.github.com>
2024-12-24 08:34:26 +0530

    large audio limit

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { json, urlencoded } from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Enable CORS globally
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ limit: '50mb', extended: true }));
   app.enableCors({
     origin: ['http://localhost:3000', 'https://admin-f2fintech.netlify.app'], // Allow requests from this origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
