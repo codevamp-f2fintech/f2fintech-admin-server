311ae640baf44943b7974f1e91c40cd7a61fe560
coder-faraz <hfaraz316@gmail.com>
2025-03-03 11:08:53 +0530

    changed employee status

--- src/tickets/entities/ticket.entity.ts
+++ src/tickets/entities/ticket.entity.ts
@@ -12,17 +12,20 @@ import { Application } from 'src/applications/entities/applications.entity';
 
 export enum Status {
   UNDER_CREDIT_REVIEW = 'under credit review',
-  TO_BE_LOGIN = 'to be login',
+  OPERATIONS = 'operations',
   PENDENCY_IN_FILE = 'pendency in file',
+  FILE_SEND_TO_BANKER = 'file send to banker',
   TO_BE_APPROVED = 'to be approved',
   TO_BE_DISBURSED = 'to be disbursed',
-  FILE_SEND_TO_BANKER = 'file send to banker',
-  TVR_DONE = 'tvr done',
-  CAM_REPORT_DONE = 'cam report done',
-  RELOOK = 'relook'
+  APPROVED = 'approved',
+  DISBURSED = 'disbursed',
+  // TO_BE_LOGIN = 'to be login',
+  // TVR_DONE = 'tvr done',
+  // CAM_REPORT_DONE = 'cam report done',
+  // RELOOK = 'relook'
 }
 
-@Entity( 'tickets' )
+@Entity('tickets')
 export class Ticket {
   @PrimaryGeneratedColumn()
   id: number;
@@ -32,58 +35,57 @@ export class Ticket {
   @Index()
   customer_application_id: number;
 
-  @OneToOne( () => Application, ( application ) => application.ticket )
-  @JoinColumn( { name: 'customer_application_id' } ) // Specify the foreign key column name
+  @OneToOne(() => Application, (application) => application.ticket)
+  @JoinColumn({ name: 'customer_application_id' }) // Specify the foreign key column name
   application: Application; // This will create the relationship with the Application entity
 
   @Column()
   @Index()
   user_id: number;
 
-  @Column( { nullable: true } )
+  @Column({ nullable: true })
   forwarded_to: number;
 
-  @Column( { nullable: true } )
+  @Column({ nullable: true })
   forwarded_by: number;
 
-  @Column( {
+  @Column({
     type: 'tinyint',
     width: 1,          // Width 1 because it’s used as a boolean-like field
     default: 0,
-  } )
+  })
   is_forwarded: number;
 
   @Column()
   original_estimate: string;
 
-  @Column( {
+  @Column({
     type: 'enum',
     enum: Status,
-  } )
+  })
   status: Status;
 
   @Column()
   voice_note_url: string;
 
-  @Column( { type: 'date', nullable: true } )
+  @Column({ type: 'date', nullable: true })
   due_date: Date;
 
-  @Column( { type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' } )
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   created_at: Date;
 
-  @Column( {
+  @Column({
     type: 'timestamp',
     default: () => 'CURRENT_TIMESTAMP',
     onUpdate: 'CURRENT_TIMESTAMP',
-  } )
+  })
   updated_at: Date;
 
   @BeforeInsert()
-  setDefaultDueDate () {
-    if ( !this.due_date )
-    {
+  setDefaultDueDate() {
+    if (!this.due_date) {
       const today = new Date();
-      today.setDate( today.getDate() + 1 );
+      today.setDate(today.getDate() + 1);
       this.due_date = today;
     }
   }
