50902cf512eeffb53dd564e04866af343827467a
shnawaz844 <your_correct_email>
2026-01-30 19:34:29 +0530

    runningLoanAmount

--- src/applications/applications.service.ts
+++ src/applications/applications.service.ts
@@ -108,6 +108,9 @@ export class ApplicationsService {
         customerLocation: customer.info?.city ?? 'No location available',
         customerState: customer.info?.state ?? 'No location available',
         companyId: application.company_id ?? 'No company',
+        hasRunningLoans: application.has_running_loans,
+        whichLoan: application.which_loan,
+        runningLoanAmount: application.running_loan_amount,
       };
     });
 
